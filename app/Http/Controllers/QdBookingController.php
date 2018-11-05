e437d95248df349ff5e1513a00a9d5aacee39170
skgit2018 <srikanth.kdg01@gmail.com>
2018-11-05 17:15:15 +0530

    book

--- app/Http/Controllers/QdBookingController.php
+++ app/Http/Controllers/QdBookingController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\QdBooking;
+use Session;
+use Auth;
 
 class QdBookingController extends Controller
 {
-    //
+   public function driverbooking() {
+
+$driverbook = new QdBooking;
+
+$booking = 
+
+
+   }
 }
