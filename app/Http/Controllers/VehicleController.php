07198f5b6e78f76429f4f449015c618de8db894f
skgit2018 <srikanth.kdg01@gmail.com>
2018-11-05 17:17:05 +0530

    v

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -19,8 +19,9 @@ class VehicleController extends Controller
         $user->reg_vehicle_type = $request->input('reg_vehicle_type');
         $user->reg_vehicle_make = $request->input('reg_vehicle_make');    
         $user->reg_vehicle_model = $request->input('reg_vehicle_model');       
+        $user->reg_vehicle_No = $request->input('reg_vehicle_No');
         $user->reg_vehicle_year = $request->input('reg_vehicle_year');
-        $user->reg_vehicle_rent_details = $request->input('reg_vehicle_rent_details');  
+      /*  $user->reg_vehicle_rent_details = $request->input('reg_vehicle_rent_details');  
         $user->reg_vehicle_agency_name = $request->input('reg_vehicle_agency_name');  
         $user->reg_vehicle_agency_address = $request->input('reg_vehicle_agency_address');  
         $user->reg_vehicle_owner_name = $request->input('reg_vehicle_owner_name');  
@@ -30,11 +31,11 @@ class VehicleController extends Controller
         $user->reg_vehicle_contact_name = $request->input('reg_vehicle_contact_name');
         $user->reg_vehicle_contact_email = $request->input('reg_vehicle_contact_email');
         $user->reg_vehicle_contact_mobile = $request->input('reg_vehicle_contact_mobile');
-        $user->reg_vehicle_contact_landline = $request->input('reg_vehicle_contact_landline');
+        $user->reg_vehicle_contact_landline = $request->input('reg_vehicle_contact_landline');*/
         $user->reg_user_status = 1;
 
         $user->save();
 
-        return redirect('/searchresults');
+        return redirect('/customerdashboard');
     }
 }
