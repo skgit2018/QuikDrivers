de0a1d00a3a559be47155105eb59c0dd3270d2b6
skgit2018 <srikanth.kdg01@gmail.com>
2018-11-05 17:11:43 +0530

    c

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -9,6 +9,9 @@ use App\Customer;
 class CustomerController extends Controller
 {
     public function Customer(Request $request){
+
+       
+          
         $user = new Customer;
 
         //Fetch The Data
@@ -16,12 +19,12 @@ class CustomerController extends Controller
         $user->reg_customer_first_name = ucwords($request->input('reg_customer_first_name'));
         $user->reg_customer_last_name = ucwords($request->input('reg_customer_last_name'));    
         $user->reg_customer_gender = $request->input('reg_customer_gender');       
-        $user->reg_customer_address = $request->input('reg_customer_address');
         $user->reg_customer_street = $request->input('reg_customer_street');  
         $user->reg_customer_location = $request->input('reg_customer_location');  
         $user->reg_customer_city = $request->input('reg_customer_city');  
         $user->reg_customer_state = $request->input('reg_customer_state');  
-        $user->reg_customer_pincode = $request->input('reg_customer_pincode');  
+        $user->reg_customer_pincode = $request->input('reg_customer_pincode');
+        $user->reg_customer_image = $request->input('reg_customer_image');   
         $user->reg_customer_emergency_name = $request->input('reg_customer_emergency_name'); 
         $user->reg_customer_emergency_email = $request->input('reg_customer_emergency_email');
         $user->reg_customer_emergency_mobile = $request->input('reg_customer_emergency_mobile');
@@ -31,5 +34,6 @@ class CustomerController extends Controller
         $user->save();
 
         return redirect('/vehicle');
+       
     }
 }
