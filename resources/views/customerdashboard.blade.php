073622a37d968c04b9352efabb73ed3ef0d8d3c5
skgit2018 <srikanth.kdg01@gmail.com>
2018-11-13 17:05:24 +0530

    customer

--- resources/views/customerdashboard.blade.php
+++ resources/views/customerdashboard.blade.php
@@ -71,7 +71,7 @@
                                  <a href="/driverdashboard"><i class="fa fa-bar-chart-o"></i>Dashboard</a>
                               </li>
                               <li class="list-group-item">
-                                 <a href="/customerprofile"><i class="fa fa-user"></i>My Profile</a>
+                                 <a href="/customerprofiles"><i class="fa fa-user"></i>My Profile</a>
                               </li>
                               <li class="list-group-item">
                                  <a href="/customerrides"><i class="fa fa-car"></i>My Rides</a>
