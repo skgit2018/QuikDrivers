ae07e53131476f90fc8cea3f7cd959a5ec0b1d18
skgit2018 <srikanth.kdg01@gmail.com>
2018-11-21 15:20:12 +0530

    customerprofile

--- app/Http/Controllers/CustomerProfileController.php
+++ app/Http/Controllers/CustomerProfileController.php
@@ -24,12 +24,12 @@ class CustomerProfileController extends Controller
 
 
         $sess_id = Session::get('userdata')['id'];
-        $customer_data = DB::select("SELECT * FROM qdm_users INNER JOIN qdm_customer_info ON qdm_users.reg_user_id = qdm_customer_info.reg_user_id INNER JOIN qdm_vehicle_info ON qdm_users.reg_user_id = qdm_vehicle_info.reg_user_id   AND qdm_users.reg_user_account_type = 'user' WHER qdm_users.reg_user_id'".$sess_id."'");
+        $customer_data = DB::select("SELECT * FROM qdm_users INNER JOIN qdm_customer_info ON qdm_users.reg_user_id = qdm_customer_info.reg_user_id INNER JOIN qdm_vehicle_info ON qdm_users.reg_user_id = qdm_vehicle_info.reg_user_id   AND qdm_users.reg_user_account_type = 'user' WHERE qdm_users.reg_user_id='".$sess_id."'");
 
 
-       /* echo"<pre>";
+        /*echo"<pre>";
         print_r($customer_data);     // also try dd($data);
-        echo "</pre>";*/
+        echo "</pre>";exit;*/
       return redirect('customerprofile')->with('customerprofiles', $customer_data); 
     }
 }
